2a624cfd5fc6bd00b073f58fc7e7ba01558558b6
ProjetEZ27 <projectendzone27@gmail.com>
2026-08-29 07:36:02 +0200

    Description de ce qui a été fait

--- app/classement/page.tsx
+++ app/classement/page.tsx
@@ -88,9 +88,7 @@ export default async function Classement() {
       <div style={{
         position: 'fixed',
         inset: 0,
-         zIndex: -1,
-        transform: 'translateZ(0)',
-        transform: 'translateZ(0)',
+        zIndex: -1,
         backgroundImage: 'url(/fonds/Fond-Classment.png)',
         backgroundSize: 'cover',
         backgroundPosition: 'right center',
